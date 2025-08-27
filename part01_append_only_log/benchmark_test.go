aa290213d6f9eafcc4445521bc66de1a565909e1
Pranjal Verma <pranjalverma0296@gmail.com>
2025-08-27 12:53:50 +0530

    add benchmark and readme for part02

---
+++ part01_append_only_log/benchmark_test.go
@@ -0,0 +1,58 @@
+package kvstorefromscratch
+
+import (
+	"fmt"
+	"testing"
+)
+
+func BenchmarkGet1MKeys(b *testing.B) {
+
+	inputData := []struct {
+		input int
+	}{
+		{input: 10000},
+		{input: 100000},
+		{input: 1000000},
+	}
+	for _, data := range inputData {
+		b.Run(fmt.Sprintf("Get-%d", data.input), func(b *testing.B) {
+			tmpDir := b.TempDir()
+			store, err := ConnectFileStore(tmpDir)
+			if err != nil {
+				b.Fatalf("ConnectFileStore failed: %v", err)
+			}
+			defer store.Close()
+
+			if err := addNItemsToKVStore(store, data.input); err != nil {
+				b.Fatalf("addNItemsToKVStore failed: %v", err)
+			}
+
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				if _, err := getNthItemFromKVStore(store, i%data.input); err != nil {
+					b.Errorf("Get failed: %v", err)
+				}
+			}
+		})
+	}
+}
+
+func addNItemsToKVStore(store Store, N int) error {
+	for i := 0; i < N; i++ {
+		key := fmt.Sprintf("key-%d", i)
+		val := fmt.Sprintf("value-%d", i)
+		if err := store.Put(key, val); err != nil {
+			return fmt.Errorf("failed to put key %s: %w", key, err)
+		}
+	}
+	return nil
+}
+
+func getNthItemFromKVStore(store Store, N int) (string, error) {
+	key := fmt.Sprintf("key-%d", N)
+	val, err := store.Get(key)
+	if err != nil {
+		return "", fmt.Errorf("failed to get key %s: %w", key, err)
+	}
+	return val, nil
+}
